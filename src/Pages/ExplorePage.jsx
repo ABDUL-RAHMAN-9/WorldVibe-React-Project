3c14331cb3c89954dacf7794200943683148f624
Abdul Rahman <abdulrahman161004@gmail.com>
2025-06-04 22:27:49 +0530

    Added ExplorePage component with ExploreStories integration.

---
+++ src/Pages/ExplorePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ExploreStories from '../Components/ExploreStories'
+
+const ExplorePage = () =>
+{
+    return (
+        <div>
+            <ExploreStories />
+        </div>
+    );
+};
+
+export default ExplorePage;

b0e8cb454f7ac13c2159e696d928e1579ec03d48
Abdul Rahman <abdulrahman161004@gmail.com>
2025-06-04 22:33:22 +0530

    Enhanced Application Structure with Routing & Theme Management
    - Integrated React Router for dynamic navigation between pages
    - Added routes for HomePage, DestinationPage, and ExplorePage
    - Implemented theme state management with dark/light mode switching
    - Ensured styling consistency across components using ThemeProvider & CssBaseline
    - Structured DrawerAppBar for persistent navigation controls
    - Improved modularity for scalability and future feature integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
-import DrawerAppBar from "./DrawerAppBar";
+import DrawerAppBar from "./Sections/DrawerAppBar";
 import HomePage from "./Pages/HomePage";
+import DestinationPage from "./Pages/DestinationPage";
+import ExplorePage from './Pages/ExplorePage'
 
 const App = () =>
 {
@@ -17,10 +20,18 @@ const App = () =>
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline /> {/* Ensure consistent theme styling across components */}
-      <DrawerAppBar onToggleTheme={toggleTheme} isDarkMode={mode === "dark"} />
-      <Box>
-        <HomePage /> {/* Main Page Render */}
-      </Box>
+
+      <Router>  {/* Enables Routing */}
+        <DrawerAppBar onToggleTheme={toggleTheme} isDarkMode={mode === "dark"} />
+        <Box>
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/destination/:id" element={<DestinationPage />}></Route>
+            <Route path="/explore" element={<ExplorePage />}></Route>
+          </Routes>
+        </Box>
+      </Router>
+
     </ThemeProvider>
   );
 };
